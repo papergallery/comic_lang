f4b0a9a181a2d5717e9cf17a71f851e128ddc9aa
papergallery <trufanov@phys.vsu.ru>
2018-11-25 18:37:25 +0300

    Other name for programm

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Test',
+      title: 'ComicLeng',
       theme: ThemeData(
         // This is the theme of your application.
         //
