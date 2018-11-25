58939911ba85f93a9a2f1705808ab2716ae1d18d
papergallery <trufanov@phys.vsu.ru>
2018-11-25 19:06:37 +0300

    New

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter ziga zagha'),
+      home: MyHomePage(title: 'Flutter ziga zagha eshe chtoto'),
     );
   }
 }
