a6282a91cad9df70dfc852b8105426370f1e58da
papergallery <trufanov@phys.vsu.ru>
2018-11-26 17:18:39 +0300

    RDDD2D

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Trufadgsgsfdfdsfsfff'),
+      home: MyHomePage(title: 'f'),
     );
   }
 }
