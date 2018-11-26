c1e7cf15143e3e467316f1f1fb7288b0d7db101d
Shulgalt <saint99.99@mail.ru>
2018-11-26 17:03:02 +0300

    sosizhopy

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'e chtoto lololololol'),
+      home: MyHomePage(title: 'e chtlolol'),
     );
   }
 }
