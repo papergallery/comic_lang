37d6fca5e699c3457b4c0acfdcded27f7b74c389
papergallery <trufanov@phys.vsu.ru>
2019-02-08 23:36:49 +0300

    Banner Animation

--- app/src/main/java/com/example/comic_lang/MainActivity.java
+++ app/src/main/java/com/example/comic_lang/MainActivity.java
@@ -5,7 +5,9 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,15 +37,35 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
     };
+    public void bannerFliper(int image){
+        ImageView imageView = new ImageView(this);
+        imageView.setImageResource(image);
+        imgBanner.addView(imageView);
+        imgBanner.setFlipInterval(6000);
+        imgBanner.setAutoStart(true);
+        imgBanner.setInAnimation(this, android.R.anim.fade_in);
+        imgBanner.setOutAnimation(this, android.R.anim.fade_out);
 
+
+    }
+    ViewFlipper imgBanner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.shop);
+        imgBanner=findViewById(R.id.imgBanner);
+
+        int sliders[]={
+                R.drawable.shop_banner1, R.drawable.shop_banner2, R.drawable.shop_banner3
+        };
+        for(int slide:sliders){
+            bannerFliper(slide);
+        }
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
 
 }
